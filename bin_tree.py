7a1a897b91647f667fd0c9a129f9e30efcd32d2b
Alex-Verg <alexvergelyuk@gmail.com>
2021-11-20 19:42:31 +0200

    Make function for beautiful output binary tree. Fix rebalance function for correct work.

--- bin_tree.py
+++ bin_tree.py
@@ -9,26 +9,84 @@ class Node:
     def _str(self):
         return "%d" % self.value
 
+    def display(self):
+        lines, *_ = self._display_aux()
+        for line in lines:
+            print(line)
+
+    def _display_aux(self):
+        """Returns list of strings, width, height, and horizontal coordinate of the root."""
+        # No child.
+        if self.right is None and self.left is None:
+            line = '%s' % self.value
+            width = len(line)
+            height = 1
+            middle = width // 2
+            return [line], width, height, middle
+
+        # Only left child.
+        if self.right is None:
+            lines, n, p, x = self.left._display_aux()
+            s = '%s' % self.value
+            u = len(s)
+            first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s
+            second_line = x * ' ' + '/' + (n - x - 1 + u) * ' '
+            shifted_lines = [line + u * ' ' for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, n + u // 2
+
+        # Only right child.
+        if self.left is None:
+            lines, n, p, x = self.right._display_aux()
+            s = '%s' % self.value
+            u = len(s)
+            first_line = s + x * '_' + (n - x) * ' '
+            second_line = (u + x) * ' ' + '\\' + (n - x - 1) * ' '
+            shifted_lines = [u * ' ' + line for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, u // 2
+
+        # Two children.
+        left, n, p, x = self.left._display_aux()
+        right, m, q, y = self.right._display_aux()
+        s = '%s' % self.value
+        u = len(s)
+        first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s + y * '_' + (m - y) * ' '
+        second_line = x * ' ' + '/' + (n - x - 1 + u + y) * ' ' + '\\' + (m - y - 1) * ' '
+        if p < q:
+            left += [n * ' '] * (q - p)
+        elif q < p:
+            right += [m * ' '] * (p - q)
+        zipped_lines = zip(left, right)
+        lines = [first_line, second_line] + [a + u * ' ' + b for a, b in zipped_lines]
+        return lines, n + m + u, max(p, q) + 2, n + u // 2
+
 
 class BTree:
 
     def __init__(self):
         self.head = None
         self.size = 0
+        self.deep = 0
+        self.values_list = []
 
     def __len__(self):
         return self.size
 
     def insert(self, data):
+        self.values_list.append(data)
         root = self.head
         if not root:
             self.head = Node(data)
             self.size += 1
+            self.deep = 0
+        d = 0
         while root:
+            d += 1
             if data < root.value:
                 if not root.left:
                     root.left = Node(data)
                     self.size += 1
+                    if d > self.deep:
+                        self.deep = d
                     return
                 else:
                     root = root.left
@@ -38,6 +96,8 @@ class BTree:
                 if not root.right:
                     root.right = Node(data)
                     self.size += 1
+                    if d > self.deep:
+                        self.deep = d
                     return
                 else:
                     root = root.right
@@ -73,28 +133,41 @@ class BTree:
         self.fix_height(node)
         bf = self.bfactor(node)
         if bf >= 2:
-            if bf < 0:
+            if self.bfactor(node.right) < 0:
                 node.right = self.right_rot(node.right)
             node = self.left_rot(node)
-#            print("-" * 50 + "\n" + "Rebalance iterations:\n" + "-" * 50)
-#            print2DUtil(self.head)
-            return node
+            return node, True
         elif bf <= -2:
-            if bf > 0:
+            if self.bfactor(node.left) > 0:
                 node.left = self.left_rot(node.left)
             node = self.right_rot(node)
-#            print("-" * 50 + "\n" + "Rebalance iterations:\n" + "-" * 50)
-#            print2DUtil(self.head)
-            return node
-        return node
+            return node, True
+        return node, False
 
     def rebalance(self, node):
+        global step
         if node is None:
-            pass
+            return None, False
         else:
-            node.left = self.rebalance(node.left)
-            node.right = self.rebalance(node.right)
-            return self.balance(node)
+            node.left, pr = self.rebalance(node.left)
+            if pr:
+                print(f"Крок {step}. Балансування вершини {pr}:")
+                tree.print_tree()
+                step += 1
+            node.right, pr = self.rebalance(node.right)
+            if pr:
+                print(f"Крок {step}. Балансування вершини {pr}:")
+                tree.print_tree()
+                step += 1
+            n, pr = self.balance(node)
+            if pr:
+                return n, node.value
+            else:
+                return n, pr
+
+    def print_tree(self):
+        self.head.display()
+        print("\n\n")
 
 
 class Heap:
@@ -125,22 +198,7 @@ class Heap:
             self.max_heapify(0)
 
     def print(self):
-        print(*self.list)
-
-
-COUNT = [5]
-
-
-def print2DUtil(root, space=0):
-    if not root:
-        return
-    space += COUNT[0]
-    print2DUtil(root.right, space)
-    print()
-    for i in range(COUNT[0], space):
-        print(end = " ")
-    print(root.value)
-    print2DUtil(root.left, space)
+        print("\n", *self.list)
 
 
 if __name__ == "__main__":
@@ -149,10 +207,21 @@ if __name__ == "__main__":
     for i in range(16):
         data = nodes[i]
         tree.insert(data)
-    print2DUtil(tree.head)
+    print("Утворене двійкове дерево без балансування:\n")
+    tree.print_tree()
+
+    step = 1
+    print("Балансування раніше створеного дерева:\n")
+    tree.head, pr = tree.rebalance(tree.head)
+    if pr:
+        print(f"Крок {step}. Балансування вершини {pr}:")
+        tree.print_tree()
+        step += 1
+    tree.head, _ = tree.rebalance(tree.head)
+    print("Збалансоване за висотою дерево:")
+    tree.print_tree()
+
+    print("Вершини дерева після пірамідального сортування:")
     heap = Heap(nodes)
     heap.heapsort()
     heap.print()
-    tree.head = tree.rebalance(tree.head)
-    tree.head = tree.rebalance(tree.head)
-    print2DUtil(tree.head)
